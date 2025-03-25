7cd5ff0e7d52e86d5fc8b8c6a194b40cbaf0320c
ArthurPerac <arthurpereira.p.2310@gmail.com>
2025-03-25 00:17:46 -0500

    modo escuro

--- public/js/main.js
+++ public/js/main.js
@@ -21,3 +21,17 @@ const navLinks = document.getElementById("navLinks");
 menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// botão de modo escuro
+let trilho = document.getElementById("trilho");
+let body = document.querySelector("body")
+let navbar = document.querySelector(".navbar")
+let hero = document.querySelector(".hero")
+
+
+trilho.addEventListener("click", ()=>{
+    trilho.classList.toggle("modoescuro")
+    body.classList.toggle("modoescuro")
+    navbar.classList.toggle("modoescuro")
+    hero.classList.toggle("modoescuro")
+})
